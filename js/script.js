12199dda587b3298c99f1103291eb02aa0ca1498
m <lylesbryaire@gmail.com>
2024-08-14 21:31:53 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,118 +1,143 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const files = [
         "../songs/1.mp3",
         "../songs/2.mp3",
-        "../songs/3.mp4",
+        "../songs/3.mp3",
+        "../songs/4.mp3",
         "../songs/5.mp3",
         "../songs/6.mp3",
         "../songs/7.mp3",
         "../songs/8.mp3",
         "../songs/9.mp3",
-        "../songs/10.mp3",
+        "../songs/10.mp3"
     ];
-    let currentsongindex = Math.floor(Math.random() * files.length);
 
-    const audioplayer = document.getElementById("musicplayer");
-    const songplayerprogressbarfill = document.querySelector(".song-player-progress-bar-fill");
-    const playpausebutton = document.getElementById("play-pause-button");
-    const songplayertime = document.querySelector(".song-player-time");
-    const nextbutton = document.getElementById("next-button");
-    const prevbutton = document.getElementById("prev-button");
+    let currentSongIndex = Math.floor(Math.random() * files.length);
 
-    function toggleplaypause() {
-        if (audioplayer.paused) {
-            audioplayer.play();
-            playpausebutton.innerHTML = '<i class="fas fa-pause"></i>';
+    const audioPlayer = document.getElementById("musicplayer");
+    const songPlayerProgressBarFill = document.querySelector(".song-player-progress-bar-fill");
+    const playPauseButton = document.getElementById("play-pause-button");
+    const songPlayerTime = document.querySelector(".song-player-time");
+    const nextButton = document.getElementById("next-button");
+    const prevButton = document.getElementById("prev-button");
+
+    function togglePlayPause() {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
         } else {
-            audioplayer.pause();
-            playpausebutton.innerHTML = '<i class="fas fa-play"></i>';
+            audioPlayer.pause();
+            playPauseButton.innerHTML = '<i class="fas fa-play"></i>';
         }
     }
 
-    function loadsong(index) {
-        audioplayer.src = files[index];
-        audioplayer.loop = true;
-    }
-
-    function playnextsong() {
-        currentsongindex = (currentsongindex + 1) % files.length;
-        loadsong(currentsongindex);
-        audioplayer.play();
-    }
-
-    function playprevioussong() {
-        currentsongindex = (currentsongindex - 1 + files.length) % files.length;
-        loadsong(currentsongindex);
-        audioplayer.play();
-    }
-
-    audioplayer.addEventListener("timeupdate", () => {
-        const { currentTime, duration } = audioplayer;
-        const progresspercent = (currentTime / duration) * 100;
-        songplayerprogressbarfill.style.width = `${progresspercent}%`;
-
-        let currentminutes = Math.floor(currentTime / 60);
-        let currentseconds = Math.floor(currentTime % 60);
-        if (currentseconds < 10) {
-            currentseconds = `0${currentseconds}`;
+    function loadSong(index) {
+        if (index >= 0 && index < files.length) {
+            audioPlayer.src = files[index];
+            audioPlayer.load();
+            playPauseButton.innerHTML = '<i class="fas fa-play"></i>';
+            songPlayerTime.textContent = 'Loading...';
+        } else {
+            playNextSong();
         }
+    }
 
-        let durationminutes = Math.floor(duration / 60);
-        let durationseconds = Math.floor(duration % 60);
-        if (durationseconds < 10) {
-            durationseconds = `0${durationseconds}`;
-        }
+    function playNextSong() {
+        currentSongIndex = (currentSongIndex + 1) % files.length;
+        loadSong(currentSongIndex);
+        audioPlayer.play();
+        playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
+    }
 
-        songplayertime.textContent = `${currentminutes}:${currentseconds} / ${durationminutes}:${durationseconds}`;
+    function playPreviousSong() {
+        currentSongIndex = (currentSongIndex - 1 + files.length) % files.length;
+        loadSong(currentSongIndex);
+        audioPlayer.play();
+        playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
+    }
+
+    audioPlayer.addEventListener("canplaythrough", () => {
+        playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
     });
 
-    playpausebutton.addEventListener("click", toggleplaypause);
-    nextbutton.addEventListener("click", playnextsong);
-    prevbutton.addEventListener("click", playprevioussong);
+    audioPlayer.addEventListener("timeupdate", () => {
+        const { currentTime, duration } = audioPlayer;
 
-    loadsong(currentsongindex);
+        if (!isNaN(currentTime) && currentTime > 0) {
+            const progressPercent = (currentTime / duration) * 100;
+            songPlayerProgressBarFill.style.width = `${progressPercent}%`;
 
-    const typedtextspan = document.querySelector(".typed");
-    const cursorspan = document.querySelector(".cursor");
+            let currentMinutes = Math.floor(currentTime / 60);
+            let currentSeconds = Math.floor(currentTime % 60);
+            if (currentSeconds < 10) {
+                currentSeconds = `0${currentSeconds}`;
+            }
 
-    const textarray = ["Full-Stacks Developer", "13 y.o"];
-    const typingdelay = 100;
-    const erasingdelay = 50;
-    const newtextdelay = 1000;
-    let textarrayindex = 0;
-    let charindex = 0;
+            let durationMinutes = Math.floor(duration / 60);
+            let durationSeconds = Math.floor(duration % 60);
+            if (durationSeconds < 10) {
+                durationSeconds = `0${durationSeconds}`;
+            }
+
+            songPlayerTime.textContent = `${currentMinutes}:${currentSeconds} / ${durationMinutes}:${durationSeconds}`;
+        }
+    });
+
+    audioPlayer.addEventListener("ended", playNextSong);
+
+    songPlayerProgressBarFill.parentElement.addEventListener("click", (e) => {
+        const progressBar = e.currentTarget;
+        const rect = progressBar.getBoundingClientRect();
+        const clickX = e.clientX - rect.left;
+        const newTime = (clickX / progressBar.offsetWidth) * audioPlayer.duration;
+        audioPlayer.currentTime = newTime;
+    });
+
+    playPauseButton.addEventListener("click", togglePlayPause);
+    nextButton.addEventListener("click", playNextSong);
+    prevButton.addEventListener("click", playPreviousSong);
+    loadSong(currentSongIndex);
+
+    const typedTextSpan = document.querySelector(".typed");
+    const cursorSpan = document.querySelector(".cursor");
+    const textArray = ["Full-Stacks Developer", "13 y.o"];
+    const typingDelay = 100;
+    const erasingDelay = 50;
+    const newTextDelay = 1000;
+    let textArrayIndex = 0;
+    let charIndex = 0;
 
     function type() {
-        if (charindex < textarray[textarrayindex].length) {
-            if (!cursorspan.classList.contains("typing")) cursorspan.classList.add("typing");
-            typedtextspan.textContent += textarray[textarrayindex].charAt(charindex);
-            charindex++;
-            setTimeout(type, typingdelay);
+        if (charIndex < textArray[textArrayIndex].length) {
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
         } else {
-            cursorspan.classList.remove("typing");
-            setTimeout(erase, newtextdelay);
+            cursorSpan.classList.remove("typing");
+            setTimeout(erase, newTextDelay);
         }
     }
 
     function erase() {
-        if (charindex > 0) {
-            if (!cursorspan.classList.contains("typing")) cursorspan.classList.add("typing");
-            typedtextspan.textContent = textarray[textarrayindex].substring(0, charindex - 1);
-            charindex--;
-            setTimeout(erase, erasingdelay);
+        if (charIndex > 0) {
+            if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
         } else {
-            cursorspan.classList.remove("typing");
-            textarrayindex++;
-            if (textarrayindex >= textarray.length) textarrayindex = 0;
-            setTimeout(type, typingdelay + 1100);
+            cursorSpan.classList.remove("typing");
+            textArrayIndex++;
+            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+            setTimeout(type, typingDelay + 1100);
         }
     }
 
-    if (textarray.length) setTimeout(type, newtextdelay + 250);
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
 
-    document.getElementById('blur-container').addEventListener('click', function() {
+    document.getElementById('blur-container').addEventListener('click', function () {
         document.getElementById('blur-container').style.display = 'none';
-        audioplayer.play();
-
+        audioPlayer.play();
+        playPauseButton.innerHTML = '<i class="fas fa-pause"></i>';
     });
 });
